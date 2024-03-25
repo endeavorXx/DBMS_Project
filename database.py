12c9aaf013a1e5d30e542f156afb49b857ef5603
vashu <vashu22606@iiitd.ac.in>
2024-03-26 01:06:54 +0530

    Desigining back-end of the application

---
+++ database.py
@@ -0,0 +1,9 @@
+import mysql.connector as sql
+
+## please use your personal sql password here
+mycon = sql.connect(host="localhost",user="root",password="123456",auth_plugin = "mysql_native_password")
+cursor = mycon.cursor()
+
+cursor.execute("use megamarket")
+cursor.execute("select* from customer")
+data = cursor.fetchall()
